79a24725fe6b3901f422bb1ef9da81dedac82025
Hertanto Irawan <hertanto.irawan@outlook.com>
2022-03-06 18:43:59 +0700

    Add signup and login

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import pg from 'pg';
 import methodOverride from 'method-override';
 import moment from 'moment';
 import cookieParser from 'cookie-parser';
+import jsSHA from 'jssha';
 import { compare } from './compare.js';
 
 // Initialise DB connection
@@ -61,8 +62,9 @@ const createNote = (req, res) => {
     return;
   }
 
-  const query = 'insert into notes (habitat, date, appearance, behavior, vocalisations, flock_size) values ($1, $2, $3, $4, $5, $6) returning id';
-  const inputData = [habitat, date, appearance, behavior, vocalisations, flockSize];
+  const query = 'insert into notes (habitat, date, appearance, behavior, vocalisations, flock_size, user_id) values ($1, $2, $3, $4, $5, $6, $7) returning id';
+  const inputData = [
+    habitat, date, appearance, behavior, vocalisations, flockSize, req.cookies.userId];
 
   pool.query(query, inputData, (err, result) => {
     if (err) {
@@ -81,7 +83,7 @@ const createNote = (req, res) => {
  * @param {Object} res Response object.
  */
 const getNotes = (req, res) => {
-  const query = 'select * from notes';
+  const query = 'select n.*, u.email from notes n left join users u on n.user_id=u.id';
 
   pool.query(query, (err, result) => {
     if (err) {
@@ -198,7 +200,7 @@ const getNoteByID = (req, res) => {
     return;
   }
 
-  const query = 'select * from notes where id=$1';
+  const query = 'select n.*, u.email from notes n left join users u on n.user_id=u.id where n.id=$1';
   const inputData = [id];
 
   pool.query(query, inputData, (err, result) => {
@@ -248,14 +250,155 @@ const deleteNoteByID = (req, res) => {
   });
 };
 
+/**
+ * Get hash value of a text.
+ * @param {*} text Text to hash.
+ * @returns Hash value of a text.
+ */
+const getHashValue = (text) => {
+  // eslint-disable-next-line new-cap
+  const shaObj = new jsSHA('SHA-512', 'TEXT', { encoding: 'UTF8' });
+  shaObj.update(text);
+  return shaObj.getHash('HEX');
+};
+
+/**
+ * Create a new user.
+ * @param {Object} req Request object.
+ * @param {Object} res Response object.
+ * @returns Redirection to login page.
+ */
+const createUser = (req, res) => {
+  const query = 'insert into users (email, password) values($1, $2)';
+
+  const hashedPassword = getHashValue(req.body.password);
+  const inputData = [req.body.email, hashedPassword];
+
+  pool.query(query, inputData, (err, result) => {
+    if (err) {
+      console.log('Error executing query', err.stack);
+      res.status(503).send(result.rows);
+      return;
+    }
+
+    res.redirect('/login');
+  });
+};
+
+/**
+ * Get user login form.
+ * @param {Object} req Request object.
+ * @param {Object} res Response object.
+ */
+const getUserLogin = (req, res) => {
+  res.render('login', { source: 'login', error: '' });
+};
+
+/**
+ * Log a user in.
+ * @param {Object} req Request object.
+ * @param {Object} res Response object.
+ * @returns Redirection to home page.
+ */
+const logUserIn = (req, res) => {
+  const query = 'select * from users where email=$1';
+  const inputData = [req.body.email];
+
+  pool.query(query, inputData, (err, result) => {
+    if (err) {
+      console.log('Error executing query', err.stack);
+      res.status(503).send(result.rows);
+      return;
+    }
+
+    if (result.rows.length === 0) {
+      // we didnt find a user with that email.
+      res.status(401).render('login', { source: 'login', error: 'The login information is incorrect.' });
+      return;
+    }
+
+    const user = result.rows[0];
+
+    if (user.password === getHashValue(req.body.password)) {
+      res.cookie('userId', user.id);
+      res.redirect('/');
+    } else {
+      // password didn't match
+      res.status(401).render('login', { source: 'login', error: 'The login information is incorrect.' });
+    }
+  });
+};
+
+/**
+ * Log a user out.
+ * @param {Object} req Request object.
+ * @param {Object} res Response object.
+ * @returns Redirection to login page.
+ */
+const logUserOut = (req, res) => {
+  res.clearCookie('userId');
+  res.redirect('/login');
+};
+
+/**
+ * Get list of notes made by a user.
+ * @param {Object} req Request object.
+ * @param {Object} res Response object.
+ */
+const getUserNotesByID = (req, res) => {
+  const { id } = req.params;
+
+  // eslint-disable-next-line no-restricted-globals
+  if (isNaN(id)) {
+    res.status(404).send('Sorry, we cannot find that!');
+    return;
+  }
+
+  const query = 'select n.*, u.email from notes n left join users u on n.user_id=u.id where n.user_id=$1';
+  const inputData = [id];
+
+  pool.query(query, inputData, (err, result) => {
+    if (err) {
+      console.log('Error executing query', err.stack);
+      res.status(503).send(result.rows);
+      return;
+    }
+    const notes = result.rows;
+
+    // sort list
+    const { sortBy, sortOrder } = req.query;
+    if (sortBy) {
+      notes.sort((first, second) => compare(first, second, sortBy, sortOrder));
+    }
+
+    // format date
+    notes.forEach((note) => {
+      note.date = moment(note.date).format('dddd, MMMM D, YYYY');
+    });
+
+    if (notes.length > 0) {
+      res.render('users', {
+        notes, source: 'root', sortBy, sortOrder,
+      });
+    } else {
+      res.status(404).send('Sorry, we cannot find that!');
+    }
+  });
+};
+
 app.get('/note', getNewNote);
 app.post('/note', createNote);
 app.get('/note/:id', getNoteByID);
 app.get('/', getNotes);
-
 app.get('/note/:id/edit', getEditNote);
 app.put('/note/:id', editNote);
 app.delete('/note/:id', deleteNoteByID);
+
+app.post('/signup', createUser);
+app.get('/login', getUserLogin);
+app.post('/login', logUserIn);
+app.delete('/note/:id', logUserOut);
+app.get('/users/:id', getUserNotesByID);
 
 // start the server listening for requests
 app.listen(process.env.PORT || 3004, () => console.log('Server is running...'));
--- init_tables.sql
+++ init_tables.sql
@@ -7,3 +7,13 @@ CREATE TABLE notes (
     vocalisations   TEXT,
     flock_size      INTEGER
 );
+
+CREATE TABLE users (
+    id              SERIAL PRIMARY KEY, 
+    email           TEXT, 
+    password        TEXT
+);
+
+ALTER TABLE notes ADD COLUMN user_id INTEGER;
+UPDATE notes SET user_id = 0;
+ALTER TABLE notes ALTER COLUMN user_id SET NOT NULL;
